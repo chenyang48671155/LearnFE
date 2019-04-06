46a1905aeb972aae6fe6ba85b6cfc7e153304ad7
chenyang <chenyang.48671155@163.com>
2019-04-06 23:34:03 +0800

    first commit

---
+++ Day2/js/test.js
@@ -0,0 +1,5 @@
+(function(){
+    a = 5;
+    console.log(window.a);
+    var a = 10;
+    console.log(a);     
---
+++ Day5/testjava/demo1/src/main/java/com/example/demo1/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String []args) {
+        System.out.println("Hello World");
+    }
+}
+}
